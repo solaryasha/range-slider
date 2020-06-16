16c78787cb07839faa15fde922e79f64b5109024
solaryasha <solaryasha@gmail.com>
2020-06-16 19:25:57 +0300

    Add mousedown event handler

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 'use strict'
 
+const minRange = document.querySelector('.left');
+const maxRange = document.querySelector('.right');
+const minValue = document.querySelector('.min-value');
+const maxValue = document.querySelector('.max-value');
+
+const startPoint = Math.ceil(minRange.getBoundingClientRect().left);
+
+minRange.addEventListener('mousedown', e => {
+const shiftX = (event.clientX -startPoint >= 0)
+  ? event.clientX -startPoint
+  : 0;
+minValue.value = shiftX;
+});
+
+maxRange.addEventListener('mousedown', e => {
+const shiftX = (event.clientX -startPoint >= 0)
+  ? event.clientX -startPoint
+  : 0;
+maxValue.value = shiftX;
+});
+
